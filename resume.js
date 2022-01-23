d035207c159a7913c89db4b5e189051de474a545
Arch4raj <97578669+Arch4raj@users.noreply.github.com>
2022-01-23 15:59:51 +0530

    Day_3 task
    For the given JSON iterate over all for loops (for, for in)
    Create your own resume data in JSON format
    Read about the difference between window, screen and document in javascript

---
+++ resume.js
@@ -0,0 +1,7 @@
+var res={"Name":"Archana","Age":"27","Address":"calicut , kerala","Occupation":"Software developer","phno":"9877654457","Email":"archanasoman93@gmail.com"}
+for(var i in res)
+console.log(i,res[i])
+//for loop
+for(var i=0;i<res.length; i++){
+    console.log(res[i])
+}
